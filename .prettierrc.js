4721f8f983744b8ef0fc6a8c99aa787b07c2ae87
Cosi Leone <cosi.leone@icloud.com>
2021-08-11 22:58:28 -0400

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  useTabs: false,
+  printWidth: 80,
+  endOfLine: 'auto',
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+}
